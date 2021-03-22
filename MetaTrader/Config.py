7758b466c1edc2ac2d076f39c7754d80576461c3
amiryan <aliamiryance@gmail.com>
2021-03-22 12:14:55 +0430

    [ADD] Fix Spread Symbols to online config

--- MetaTrader/Config.py
+++ MetaTrader/Config.py
@@ -8,8 +8,13 @@ class Config:
     secondary_timefarmes = {"H12": "H4"}
     secondary_timefarmes_ratio = {"H12": 3}
     symbols_dict = {'EURUSD.I': 0, 'GBPUSD.I': 1, 'NZDUSD.I': 2, 'USDCAD.I': 3, 'USDCHF.I': 4, 'USDJPY.I': 5,
-                    'AUDUSD.I': 6, 'XAUUSD.I': 7, 'XAGUSD.I': 8, 'US30.JUN1': 9}
+                    'AUDUSD.I': 6, 'XAUUSD.I': 7, 'XAGUSD.I': 8, 'US30.JUN1': 9, 'EURUSD': 10, 'GBPUSD': 11,
+                    'NZDUSD': 12, 'USDCAD': 13, 'USDCHF': 14, 'USDJPY': 15,'AUDUSD': 16, 'GOLD': 17,
+                    'SILVER': 18, 'US30.MAY1': 19}
     symbols_list = ['EURUSD.I', 'GBPUSD.I', 'NZDUSD.I', 'USDCAD.I', 'USDCHF.I', 'USDJPY.I', 'AUDUSD.I',
-                    'XAUUSD.I', 'XAGUSD.I', 'US30.JUN1']
+                    'XAUUSD.I', 'XAGUSD.I', 'US30.JUN1', 'EURUSD', 'GBPUSD', 'NZDUSD', 'USDCAD',
+                    'USDCHF', 'USDJPY', 'AUDUSD', 'GOLD', 'SILVER', 'US30.MAY1']
     symbols_pip = {'EURUSD.I': 5, 'GBPUSD.I': 5, 'NZDUSD.I': 5, 'USDCAD.I': 5, 'USDCHF.I': 5,
-                     'USDJPY.I': 3, 'AUDUSD.I': 5, 'XAUUSD.I': 2, 'XAGUSD.I': 3, 'US30.JUN1': 0}
+                     'USDJPY.I': 3, 'AUDUSD.I': 5, 'XAUUSD.I': 2, 'XAGUSD.I': 3, 'US30.JUN1': 0,
+                   'EURUSD': 5, 'GBPUSD': 5, 'NZDUSD': 5, 'USDCAD': 5, 'USDCHF': 5,
+                   'USDJPY': 3, 'AUDUSD': 5, 'GOLD': 2, 'SILVER': 3, 'US30.MAY1': 1}
